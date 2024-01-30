ee0a810688bd79a95651dcd4eb1ceb3f2c4644e2
yujin <youjin010402@naver.com>
2024-01-30 10:49:39 +0900

    'geolocation commit'good test

--- src/component/axios/LoginRequest.js
+++ src/component/axios/LoginRequest.js
@@ -3,3 +3,6 @@ import request from "./axios";
 export const LoginGoogle = async (data) => {
   return await request.post(`/login/oauth2/code/google?token=${data}`);
 };
+export const LoginGoogle = async (data) => {
+  return await request.post(`/login/oauth2/code/google?token=${data}`);
+};
